67f1c07d51ad7869007c4ca3e5c73e519e0d79c5
Horus Dev <horusm99@gmail.com>
2020-03-21 20:00:32 +0300

    Create files

---
+++ app/Http/Controllers/Api/v1/Filemanager/itemsController.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace App\Http\Controllers\Api\v1\Filemanager;
+
+
+class itemsController
+{
+
+}
---
+++ app/Http/Controllers/Filemanager/FilemanagerItem.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace App\Http\Controllers\Filemanager;
+
+
+class FilemanagerItem
+{
+
+}
