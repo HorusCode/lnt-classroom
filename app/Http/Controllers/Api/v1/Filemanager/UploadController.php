351d2389889798579e4c207d2d2907eb28172c6d
Horus Dev <horusm99@gmail.com>
2020-03-23 23:22:17 +0300

    UploadController.php created

--- app/Http/Controllers/Api/v1/Filemanager/UploadController.php
+++ app/Http/Controllers/Api/v1/Filemanager/UploadController.php
@@ -4,7 +4,47 @@
 namespace App\Http\Controllers\Api\v1\Filemanager;
 
 
-class UploadController
-{
+use Log;
 
+class UploadController extends FilemanagerController
+{
+    protected $errors;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->errors = [];
+    }
+
+    public function upload()
+    {
+        $uploaded_files = request()->file('upload');
+        $error_bag = [];
+        $new_filename = null;
+
+        foreach (is_array($uploaded_files) ? $uploaded_files : [$uploaded_files] as $file) {
+            try {
+                $new_filename = $this->filemanager->upload();
+            } catch (\Exception $exception) {
+                Log::error($exception->getMessage(), [
+                    'file' => $exception->getFile(),
+                    'line' => $exception->getLine(),
+                    'trace' => $exception->getTraceAsString()
+                ]);
+                array_push($error_bag, $exception->getMessage());
+            }
+        }
+
+        if(is_array($uploaded_files)) {
+            $response = count($error_bag) > 0 ? $error_bag : [parent::$success_response];
+        } else {
+            if(is_null($new_filename)) {
+                $response = $error_bag[0];
+            } else {
+                $response = $this->filemanager->setName($new_filename)->url();
+            }
+        }
+
+        return response()->json(['data' => $response]);
+    }
 }
