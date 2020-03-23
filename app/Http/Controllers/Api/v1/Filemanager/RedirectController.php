83113d1ff10d6cb476e401e8604826e879358e5d
Horus Dev <horusm99@gmail.com>
2020-03-23 21:03:01 +0300

    Create RedirectController.php

--- app/Http/Controllers/Api/v1/Filemanager/RedirectController.php
+++ app/Http/Controllers/Api/v1/Filemanager/RedirectController.php
@@ -4,7 +4,17 @@
 namespace App\Http\Controllers\Api\v1\Filemanager;
 
 
-class RedirectController
-{
+use Storage;
 
+class RedirectController extends FilemanagerController
+{
+    public function showFile($file_path)
+    {
+        $storage = Storage::disk($this->helper->config('disk'));
+        if (!$storage->exists($file_path)) {
+            abort(404);
+        }
+
+        return response($storage->get($file_path))->header('Content-Type', $storage->mimeType($file_path));
+    }
 }
